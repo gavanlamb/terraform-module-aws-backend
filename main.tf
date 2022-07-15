37a92a768027b0cef4ed69621c2d5492a105c4cc
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-07-16 00:16:41 +1000

    Change aws_s3_account_public_access_block to aws_s3_bucket_public_access_block

--- main.tf
+++ main.tf
@@ -54,7 +54,8 @@ resource "aws_s3_bucket" "remote_state" {
     }
   }
 }
-resource "aws_s3_account_public_access_block" "remote_state" {
+resource "aws_s3_bucket_public_access_block" "remote_state" {
+  bucket = aws_s3_bucket.remote_state.id
   block_public_acls = true
   block_public_policy = true
   restrict_public_buckets = true
