2437a598565bfb4246729c18d2013fd9fce66539
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-07-16 00:38:52 +1000

    Add kms key

--- main.tf
+++ main.tf
@@ -85,10 +85,94 @@ resource "aws_dynamodb_table" "lock_table" {
 
   server_side_encryption { 
     enabled = true
+    kms_key_arn = aws_kms_alias.terraform_key.arn
   }
   
   lifecycle {
     prevent_destroy = true
   }
   tags = merge(local.default_tags, var.dynamodb_tags)
+}
+
+resource "aws_kms_key" "terraform_key" {
+  description = "KMS key for cloudwatch"
+  deletion_window_in_days = 10
+  policy = data.aws_iam_policy_document.terraform.json
+}
+
+resource "aws_kms_alias" "terraform_key" {
+  name = "alias/expensely/${lower(var.environment)}/${var.name}"
+  target_key_id = aws_kms_key.terraform_key.key_id
+}
+data "aws_iam_policy_document" "terraform_key" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      "*"
+    ]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    resources = [
+      "*"
+    ]
+    principals {
+      type = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = [
+        "monitoring.${data.aws_region.current.name}.amazonaws.com"
+      ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = [
+        data.aws_caller_identity.current.account_id
+      ]
+    }
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    resources = [
+      "*"
+    ]
+    principals {
+      type = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = [
+        "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*"
+      ]
+    }
+  }
 }
